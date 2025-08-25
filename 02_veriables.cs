db295eeff47337a6f9a308ce5b88dec3aac59a83
Mehmet Emin Deniz <mehmetemindenizofficial@gmail.com>
2025-08-25 15:54:47 +0300

    Update 02_veriables.cs

--- 02_veriables.cs
+++ 02_veriables.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,51 +11,51 @@ namespace _02_Veriables
         static void Main(string[] args)
         {
             #region Double Değişkenler
-            //// Double Değişkenler
-            ////double number;
-            ////number = 4.87;
-            ////Console.WriteLine(number);
-            //// burda araya konan nokta cmd ekranında virgül olarak çıktı verir.
+            // Double Değişkenler
+            double number;
+            number = 4.87;
+            Console.WriteLine(number);
+            // burda araya konan nokta cmd ekranında virgül olarak çıktı verir.
 
-            //double elmaKilo, havucKilo, cilekKilo, patatesKilo, domatesKilo;
-            //elmaKilo = 14.85;
-            //havucKilo = 20.95;
-            //cilekKilo = 45;
-            //patatesKilo = 9.74;
-            //domatesKilo = 6.88;
+            double elmaKilo, havucKilo, cilekKilo, patatesKilo, domatesKilo;
+            elmaKilo = 14.85;
+            havucKilo = 20.95;
+            cilekKilo = 45;
+            patatesKilo = 9.74;
+            domatesKilo = 6.88;
 
-            //Console.WriteLine("----- Fiyat Listesi -----");
-            //Console.WriteLine();
-            //Console.WriteLine("----- Elma Birim Fiyatı:" + elmaKilo);
-            //Console.WriteLine("----- Havuç Birim Fiyatı:" + havucKilo);
-            //Console.WriteLine("----- Çilek Birim Fiyatı:" + cilekKilo);
-            //Console.WriteLine("----- Patates Birim Fiyatı:" + patatesKilo);
-            //Console.WriteLine("----- Domates Birim Fiyatı:" +domatesKilo);
-            //Console.WriteLine() ; Console.WriteLine();
+            Console.WriteLine("----- Fiyat Listesi -----");
+            Console.WriteLine();
+            Console.WriteLine("----- Elma Birim Fiyatı:" + elmaKilo);
+            Console.WriteLine("----- Havuç Birim Fiyatı:" + havucKilo);
+            Console.WriteLine("----- Çilek Birim Fiyatı:" + cilekKilo);
+            Console.WriteLine("----- Patates Birim Fiyatı:" + patatesKilo);
+            Console.WriteLine("----- Domates Birim Fiyatı:" + domatesKilo);
+            Console.WriteLine(); Console.WriteLine();
 
-            //// Kaçar gram alınacak? :
-            //double elmaGram, havucGram, cilekGram, patatesGram, domatesGram;
-            //elmaGram = 1.245;
-            //havucGram = 2.650;
-            //cilekGram = 0.750;
-            //patatesGram = 4.859;
-            //domatesGram = 3.745;
+            // Kaçar gram alınacak? :
+            double elmaGram, havucGram, cilekGram, patatesGram, domatesGram;
+            elmaGram = 1.245;
+            havucGram = 2.650;
+            cilekGram = 0.750;
+            patatesGram = 4.859;
+            domatesGram = 3.745;
 
-            //double elmaToplam = elmaGram*elmaKilo;
-            //double havucToplam = havucGram*havucKilo;
-            //double cilekToplam = cilekGram*cilekKilo;
-            //double patatesToplam = patatesGram*patatesKilo;
-            //double domatesToplam = domatesGram*domatesKilo;
+            double elmaToplam = elmaGram * elmaKilo;
+            double havucToplam = havucGram * havucKilo;
+            double cilekToplam = cilekGram * cilekKilo;
+            double patatesToplam = patatesGram * patatesKilo;
+            double domatesToplam = domatesGram * domatesKilo;
 
-            //Console.WriteLine("Elma birim fiyatı: " + elmaKilo + " *** alınan elma miktarı: " + elmaGram + " *** toplam elma fiyatı:" + elmaToplam);
-            //Console.WriteLine("Havuç birim fiyatı: " + havucKilo + " *** alınan havuç miktarı: " + havucGram + " *** toplam havuç fiyatı:" + havucToplam);
-            //Console.WriteLine("Çilek birim fiyatı: " + cilekKilo + " *** alınan çilek miktarı: " + cilekGram + " *** toplam çilek fiyatı:" + cilekToplam);
-            //Console.WriteLine("Patates birim fiyatı: " + patatesKilo + " *** alınan patates miktarı: " + patatesGram + " *** toplam patates fiyatı:" + patatesToplam);
-            //Console.WriteLine("Domates birim fiyatı: " + domatesKilo + " *** alınan domates miktarı: " + domatesGram + " *** toplam domates fiyatı:" + domatesToplam);
-            //Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("Elma birim fiyatı: " + elmaKilo + " *** alınan elma miktarı: " + elmaGram + " *** toplam elma fiyatı:" + elmaToplam);
+            Console.WriteLine("Havuç birim fiyatı: " + havucKilo + " *** alınan havuç miktarı: " + havucGram + " *** toplam havuç fiyatı:" + havucToplam);
+            Console.WriteLine("Çilek birim fiyatı: " + cilekKilo + " *** alınan çilek miktarı: " + cilekGram + " *** toplam çilek fiyatı:" + cilekToplam);
+            Console.WriteLine("Patates birim fiyatı: " + patatesKilo + " *** alınan patates miktarı: " + patatesGram + " *** toplam patates fiyatı:" + patatesToplam);
+            Console.WriteLine("Domates birim fiyatı: " + domatesKilo + " *** alınan domates miktarı: " + domatesGram + " *** toplam domates fiyatı:" + domatesToplam);
+            Console.WriteLine(); Console.WriteLine();
 
-            //double toplamTutar = elmaToplam+havucToplam+cilekToplam+patatesToplam+domatesToplam;
-            //Console.WriteLine("Toplam tutar: " + toplamTutar);
+            double toplamTutar = elmaToplam+havucToplam+cilekToplam+patatesToplam+domatesToplam;
+            Console.WriteLine("Toplam tutar: " + toplamTutar);
             #endregion
 
             #region Char Değişkenler
@@ -65,42 +65,42 @@ namespace _02_Veriables
             // Charlar tek tek harfleri ele almamız gerektiğinde kullanılır
             // tek tırnak içinde tanımlanır 
 
-            //char karakter;
-            //karakter = 'a';
-            //Console.WriteLine(karakter);
+            char karakter;
+            karakter = 'a';
+            Console.WriteLine(karakter);
 
             #endregion
 
             #region Klavyeden Veri Girişleri String Değişkenler
 
-            //Console.WriteLine("*** C# Hava Yolları Yolcu Bilgisi ***");
-            //string musteriAdi, musteriSoyadi, musteriSehir, musteriYas, musteriTc;
-            //// bunları klavyeden almak için;
+            Console.WriteLine("*** C# Hava Yolları Yolcu Bilgisi ***");
+            string musteriAdi, musteriSoyadi, musteriSehir, musteriYas, musteriTc;
+            // bunları klavyeden almak için;
 
-            //Console.Write("Yolcu Adı: ");
-            //musteriAdi= Console.ReadLine();
+            Console.Write("Yolcu Adı: ");
+            musteriAdi= Console.ReadLine();
 
-            //Console.Write("Yolcu Soyadı: ");
-            //musteriSoyadi = Console.ReadLine();
+            Console.Write("Yolcu Soyadı: ");
+            musteriSoyadi = Console.ReadLine();
 
-            //Console.Write("Yolcu Sehir: ");
-            //musteriSehir = Console.ReadLine();
+            Console.Write("Yolcu Sehir: ");
+            musteriSehir = Console.ReadLine();
 
-            //Console.Write("Yolcu Yaş: ");
-            //musteriYas = Console.ReadLine();
+            Console.Write("Yolcu Yaş: ");
+            musteriYas = Console.ReadLine();
 
-            //Console.Write("Yolcu Tc No: ");
-            //musteriTc = Console.ReadLine();
+            Console.Write("Yolcu Tc No: ");
+            musteriTc = Console.ReadLine();
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Console.WriteLine("------------------------");
-            //Console.WriteLine(
-            //    "Yolcu Tc No: "+musteriTc+" "+
-            //    "Yolcu Ad Soyad: " + musteriAdi +" " + musteriSoyadi+" "+
-            //    "Yolcu Şehir: "+musteriSehir+" "+
-            //    "Yolcu Yaş: "+" "+musteriYas
-            //    );
+            Console.WriteLine("------------------------");
+            Console.WriteLine(
+                "Yolcu Tc No: "+musteriTc+" "+
+                "Yolcu Ad Soyad: " + musteriAdi +" " + musteriSoyadi+" "+
+                "Yolcu Şehir: "+musteriSehir+" "+
+                "Yolcu Yaş: "+" "+musteriYas
+                );
 
 
 
@@ -114,7 +114,7 @@ namespace _02_Veriables
             // girilen şeyi stringden integer'a dönüştürmemiz gerekiyor
             // girilen şeyin sayı olduğunu bana belirt diyor C#
 
-            /* int ayakkabiFiyati, bilgisayarFiyati, sandalyeFiyati, tvFiyati;
+             int ayakkabiFiyati, bilgisayarFiyati, sandalyeFiyati, tvFiyati;
             ayakkabiFiyati = 1000;
             bilgisayarFiyati = 20000;
             sandalyeFiyati = 5000;
@@ -144,12 +144,11 @@ namespace _02_Veriables
             Console.WriteLine("Toplam tutar: " + toplamFiyat);
 
             // pars dönüşüm için kullanılan bir method 
-            */
 
             #endregion
 
             #region Klavyeden Ondalıklı Sayı İşlemleri
-            /* 
+            
             double sınav1, sınav2, sınav3, ortalama;
 
             Console.Write("1. Sınav Notunuzu Giriniz: ");
@@ -167,16 +166,18 @@ namespace _02_Veriables
             Console.WriteLine("Sınav Ortalamanız: "+ortalama);
 
             // kodlamada ondalıklı girerken nokta koyarız
-            // cmd'de noktalı girerken virgül koyarız */
+            // cmd'de noktalı girerken virgül koyarız 
             #endregion
 
             #region Klavyeden Karakter Girişleri
 
-            char cinsiyet;
-            Console.Write("Lütfen Cinsiyet Seçiniz: ");
-            cinsiyet = char.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine("Cinsiyetiniz :" + cinsiyet);
+            
+             char cinsiyet;
+             Console.Write("Lütfen Cinsiyet Seçiniz: ");
+             cinsiyet = char.Parse(Console.ReadLine());
+             Console.WriteLine();
+             Console.WriteLine("Cinsiyetiniz :" + cinsiyet);
+            
 
             #endregion
 
